00ed7b1a9ced67d16e3eedd5ad6ed4c87b658ff6
Rishabh Jain <jainrishabh2610@gmail.com>
2026-01-03 21:19:19 +0530

    updates

--- backend/socket.js
+++ backend/socket.js
@@ -7,7 +7,9 @@ const server = http.createServer(app);//quick http server creation using express
 
 const io=new Server(server,{
   cors:{
-    origin:"http://localhost:5173",//frontend server
+    origin:["http://localhost:5173",//frontend server
+    "https://vibeshare-hj0f.onrender.com"
+  ],
     methods:["GET","POST","PATCH","DELETE"]
   }
 });//socket io server creation
